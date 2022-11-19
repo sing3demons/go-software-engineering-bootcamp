17d31233ed3de28a6bb649efdb797ee041605037
sing3demons <sing3demons@live.com>
2022-11-19 20:45:01 +0700

    :memo: docs: go test coverage

--- ticket/ticket_test.go
+++ ticket/ticket_test.go
@@ -15,7 +15,8 @@ func TestTicketPrice(t *testing.T) {
 		{name: "free Ticket when age under 3", age: 3, want: 0.0},
 		{name: "Ticket $15 when age at 4 year old", age: 4, want: 15.0},
 		{name: "Ticket $15 when age at 15 year old", age: 15, want: 15.0},
-		{name: "Ticket $30 when age over 16", age: 16, want: 30.0},
+		{name: "Ticket $30 when age over 15", age: 16, want: 30.0},
+		{name: "Ticket $30 when age is 50", age: 50, want: 30.0},
 		{name: "Ticket $5 when age over 50", age: 51, want: 5.0},
 	}
 
