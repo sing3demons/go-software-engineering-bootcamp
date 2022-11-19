c48f2496c342138024e3dfcf29030a33a97460cb
sing3demons <sing3demons@live.com>
2022-11-19 20:52:02 +0700

    :white_check_mark: test: black boc testing

---
+++ ticket/ticket_external_test.go
@@ -0,0 +1,36 @@
+package ticket_test
+
+import (
+	"testing"
+
+	"github.com/sing3demons/softq/ticket"
+)
+
+type testCase struct {
+	name string
+	age  int
+	want float64
+}
+
+func TestTicketPrice(t *testing.T) {
+
+	tests := []testCase{
+		{name: "should return 0 when age is 0", age: 0, want: 0.0},
+		{name: "free Ticket when age under 3", age: 3, want: 0.0},
+		{name: "Ticket $15 when age at 4 year old", age: 4, want: 15.0},
+		{name: "Ticket $15 when age at 15 year old", age: 15, want: 15.0},
+		{name: "Ticket $30 when age over 15", age: 16, want: 30.0},
+		{name: "Ticket $30 when age is 50", age: 50, want: 30.0},
+		{name: "Ticket $5 when age over 50", age: 51, want: 5.0},
+	}
+
+	for _, tc := range tests {
+		t.Run(tc.name, func(t *testing.T) {
+			got := ticket.Price(uint(tc.age))
+
+			if got != tc.want {
+				t.Errorf("Price(%d) = %f; want %f", tc.age, got, tc.want)
+			}
+		})
+	}
+}
