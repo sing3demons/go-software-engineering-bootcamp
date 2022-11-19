36112ef1c3317d8e6d31c35b268373cb3c8fa041
sing3demons <sing3demons@live.com>
2022-11-19 13:46:39 +0700

    :recycle: aaa test pattern

--- README.md
+++ README.md
@@ -3,3 +3,22 @@
 ```all test case
 go test -v ./...
 ```
+
+## Arrange, Act, Assert pattern
+
+```example test pattern Go
+func TestSum1(t *testing.T) {
+	t.Run("should return 3 when 1 and 2", func(t *testing.T) {
+		// Arrange
+		want := 3
+
+		// Act
+		got := sum(1, 2)
+
+		// Assert
+		if got != want {
+			t.Errorf("sum(1,2) = %d; want %d", got, want)
+		}
+	})
+}
+```
--- sum/sum_test.go
+++ sum/sum_test.go
@@ -3,25 +3,36 @@ package sum
 import "testing"
 
 func TestSum(t *testing.T) {
+	// Arrange, Act, Assert pattern
 	t.Run("should return 3 when 1 and 2", func(t *testing.T) {
+		// Arrange
+		want := 3
+
+		// Act
 		got := sum(1, 2)
-		if got != 3 {
-			t.Errorf("sum(1,2) = %d; want 3", got)
+
+		// Assert
+		if got != want {
+			t.Errorf("sum(1,2) = %d; want %d", got, want)
 		}
 	})
 
 	t.Run("should return 1 when 1 and 0", func(t *testing.T) {
+		want := 1
+
 		got := sum(1, 0)
 
-		if got != 1 {
-			t.Errorf("sum(1, 0) = %d; want 1", got)
+		if got != want {
+			t.Errorf("sum(1, 0) = %d; want %d", got, want)
 		}
 	})
 	t.Run("should return -2 when -1 and -1", func(t *testing.T) {
+		want := -2
+		
 		got := sum(-1, -1)
 
-		if got != -2 {
-			t.Errorf("sum(-1, -1) = %d; want -2", got)
+		if got != want {
+			t.Errorf("sum(-1, -1) = %d; want %d", got, want)
 		}
 	})
 }
