7f8860ed5ff3a6ad6c0698ccd04dfc8b32951f9a
sing3demons <sing3demons@live.com>
2022-11-19 20:23:06 +0700

    :recycle: create struct

--- ticket/ticket_test.go
+++ ticket/ticket_test.go
@@ -2,19 +2,21 @@ package ticket
 
 import "testing"
 
+type testCase struct {
+	name string
+	age  int
+	want float64
+}
+
 func TestTicketPrice(t *testing.T) {
 
-	tests := []struct {
-		name string
-		age  int
-		want float64
-	}{
-		{"should return 0 when age is 0", 0, 0.0},
-		{"free Ticket when age under 3", 3, 0.0},
-		{"Ticket $15 when age at 4 year old", 4, 15.0},
-		{"Ticket $15 when age at 15 year old", 15, 15.0},
-		{"Ticket $30 when age over 16", 16, 30.0},
-		{"Ticket $5 when age over 50", 51, 5.0},
+	tests := []testCase{
+		{name: "should return 0 when age is 0", age: 0, want: 0.0},
+		{name: "free Ticket when age under 3", age: 3, want: 0.0},
+		{name: "Ticket $15 when age at 4 year old", age: 4, want: 15.0},
+		{name: "Ticket $15 when age at 15 year old", age: 15, want: 15.0},
+		{name: "Ticket $30 when age over 16", age: 16, want: 30.0},
+		{name: "Ticket $5 when age over 50", age: 51, want: 5.0},
 	}
 
 	for _, tc := range tests {
