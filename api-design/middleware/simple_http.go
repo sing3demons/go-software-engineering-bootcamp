17ecad7ae7530403d6f431d0fb8ed038949f4014
sing3demons <sing3demons@live.com>
2022-11-20 21:19:35 +0700

    :sparkles: simple http middleware

---
+++ api-design/middleware/simple_http.go
@@ -0,0 +1,72 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"io"
+	"log"
+	"net/http"
+)
+
+type User struct {
+	ID   int    `json:"id"`
+	Name string `json:"name"`
+	Age  int    `json:"age"`
+}
+
+var users []User = []User{{ID: 1, Name: "sing", Age: 21}}
+
+func healthHandler(w http.ResponseWriter, r *http.Request) {
+	w.WriteHeader(http.StatusOK)
+	w.Write([]byte("OK"))
+}
+
+func main() {
+	http.HandleFunc("/users", usersHandle)
+	http.HandleFunc("/health", healthHandler)
+
+	log.Println("Server started as :2565")
+	log.Fatal(http.ListenAndServe(":2565", nil))
+}
+
+func usersHandle(w http.ResponseWriter, r *http.Request) {
+	if r.Method == http.MethodGet {
+
+		b, err := json.Marshal(users)
+		if err != nil {
+			w.WriteHeader(http.StatusInternalServerError)
+			w.Write([]byte(err.Error()))
+		}
+		w.Write(b)
+		return
+	}
+
+	if r.Method == http.MethodPost {
+		body, err := io.ReadAll(r.Body)
+		if err != nil {
+			w.WriteHeader(http.StatusInternalServerError)
+			fmt.Fprintf(w, "error : %v", err)
+			return
+		}
+
+		id := len(users) + 1
+
+		user := User{}
+		user.ID = id
+
+		err = json.Unmarshal(body, &user)
+		if err != nil {
+			w.WriteHeader(http.StatusBadRequest)
+			fmt.Fprintf(w, "error : %v", err)
+			return
+		}
+
+		users = append(users, user)
+
+		// w.Write(body)
+		fmt.Fprintf(w, "create user")
+		return
+	}
+
+	w.WriteHeader(http.StatusMethodNotAllowed)
+}
